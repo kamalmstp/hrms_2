8782299e1f34d05fe9fe928eb4299a5ae5b35762
kamal <mustapakamalkml@gmail.com>
2021-04-02 14:25:41 +0800

    type css

--- app/Views/layout/index.php
+++ app/Views/layout/index.php
@@ -12,37 +12,37 @@
     <title><?= $title; ?> | HRMS </title>
 
     <!-- Bootstrap -->
-    <link href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- NProgress -->
-    <link href="/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
-    <link href="/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
     <!-- bootstrap-daterangepicker -->
-    <link href="/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
     <!-- PNotify -->
-    <link href="/vendors/pnotify/dist/pnotify.css" rel="stylesheet">
-    <link href="/vendors/pnotify/dist/pnotify.buttons.css" rel="stylesheet">
-    <link href="/vendors/pnotify/dist/pnotify.nonblock.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/pnotify/dist/pnotify.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/pnotify/dist/pnotify.buttons.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/pnotify/dist/pnotify.nonblock.css" rel="stylesheet">
     <!-- bootstrap-wysiwyg -->
-    <link href="/vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
     <!-- Select2 -->
-    <link href="/vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/select2/dist/css/select2.min.css" rel="stylesheet">
     <!-- Switchery -->
-    <link href="/vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/switchery/dist/switchery.min.css" rel="stylesheet">
     <!-- starrr -->
-    <link href="/vendors/starrr/dist/starrr.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/starrr/dist/starrr.css" rel="stylesheet">
     <!-- bootstrap-daterangepicker -->
-    <link href="/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
     <!-- Datatables -->
-    <link href="/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-    <link href="/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-    <link href="/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-    <link href="/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-    <link href="/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
     <!-- Custom Theme Style -->
-    <link href="/build/css/custom.min.css" rel="stylesheet">
+    <link type="text/css" href="/build/css/custom.min.css" rel="stylesheet">
 </head>
 
 <body class="nav-md">
